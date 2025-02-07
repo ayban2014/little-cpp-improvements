37ec2d92f5f6e078aaf6e4244863ec42ca89b9e6
ayban <ayanbansal4@gmail.com>
2025-02-07 15:32:20 -0500

    Update thelittlethings.cpp

--- thelittlethings.cpp
+++ thelittlethings.cpp
@@ -11,12 +11,14 @@ template<typename flexible>
 void give(flexible i) {
     std::cout << i;
 }
+template<typename flexible>
 void get(flexible& i, std::optional<std::string> o) {
     if (o) {
         std::cout << *o;
     }
     std::cin >> i;
 }
+template<typename flexible>
 void givebool(bool i) {
     if (i) {
         std::cout << "true";
@@ -24,18 +26,23 @@ void givebool(bool i) {
         std::cout << "false";
     }
 }
+template<typename flexible>
 std::string as_str(flexible i) {
     return std::to_string(i);
 }
+template<typename flexible>
 void vectorappend(std::vector<flexible>& v, flexible i) {
     v.push_back(i);
 }
+template<typename flexible>
 void end() {
     std::cout << std::endl;
 }
+template<typename flexible>
 int randint(int i) {
     return rand() % i;
 }
+template<typename flexible>
 V mapfind(const std::map<K, V>& map, const K& item) {
     auto it = map.find(item);
     if (it != map.end()) {
@@ -44,15 +51,19 @@ V mapfind(const std::map<K, V>& map, const K& item) {
         return V();
     }
 }
+template<typename flexible>
 int as_int(flexible i) {
     return static_cast<int>(i);
 }
+template<typename flexible>
 bool as_bool(flexible i) {
     return static_cast<bool>(i);
 }
+template<typename flexible>
 double sigma(const std::vector<double>& vec) {
     return std::accumulate(vec.begin(), vec.end(), 0.0);
 }
+template<typename flexible>
 flexible randchoice(const std::vector<flexible>& vec) {
     if (vec.empty()) {
         throw std::runtime_error("Cannot choose from an empty vector");
@@ -62,6 +73,7 @@ flexible randchoice(const std::vector<flexible>& vec) {
     std::uniform_int_distribution<> dis(0, vec.size() - 1);
     return vec[dis(gen)];
 }
+template<typename flexible>
 std::vector<double> range(l, h) {
 	std::vector<double> r;
 	for (int i = l; i <= h; i++) {
@@ -69,6 +81,7 @@ std::vector<double> range(l, h) {
 	}
 	return r;
 }
+template<typename flexible>
 std::vector<double> pmtd(double n1, double n2) {
 	return {n1+n2, n1-n2, n1*n2, n1/n2, pow(n1, n2)};
 }
