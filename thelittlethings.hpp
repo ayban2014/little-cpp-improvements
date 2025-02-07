c42e27f52a97fc4b5689ed8e01f3eafa0a445ed9
ayban <ayanbansal4@gmail.com>
2025-02-07 15:29:56 -0500

    Update thelittlethings.hpp

--- thelittlethings.hpp
+++ thelittlethings.hpp
@@ -13,16 +13,27 @@ template<typename flexible>
 void give(flexible i);
 void get(std::optional<std::string> o, flexible& i);
 void givebool(bool i);
+template<typename flexible>
 std::string as_str(flexible i);
+template<typename flexible>
 void vectorappend(std::vector<flexible>& v, flexible i);
+template<typename flexible>
 void end();
+template<typename flexible>
 int randint(int i);
+template<typename flexible>
 template<typename K, typename V>
 V mapfind(const std::map<K, V>& map, const K& item);
+template<typename flexible>
 int as_int(flexible i);
+template<typename flexible>
 bool as_bool(flexible i);
+template<typename flexible>
 double sigma(const std::vector<double>& vec);
+template<typename flexible>
 flexible randchoice(const std::vector<flexible>& vec);
+template<typename flexible>
 std::vector<double> range(l, h);
+template<typename flexible>
 std::vector<double> pmtd(double n1, double n2);
 #endif
